436a619c0961c7e35637239f530b9aad53725d6f
MatrixMyRIP <lolariana31@gmail.com>
2024-11-30 08:30:59 +0300

    lab2 - b_add_signals done

--- lab1/a_add_ui.py
+++ lab1/a_add_ui.py
@@ -8,7 +8,7 @@ PySide6-uic path_to_form.ui -o path_to_form.py
 from PySide6 import QtWidgets
 
 # from ui.login import Ui_Form  # Импортируем класс формы
-from ui.calcul import Ui_Form
+from ui.book_choise import Ui_Form
 
 class Window(QtWidgets.QWidget):  # наследуемся от того же класса, что и форма в QtDesigner
     def __init__(self, parent=None):
