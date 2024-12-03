1d748ea66f383d197b7d78a4abe8650cf313d96c
MatrixMyRIP <lolariana31@gmail.com>
2024-12-04 00:19:49 +0300

    lab2 - с_signal_done

--- lab2/c_signals_events.py
+++ lab2/c_signals_events.py
@@ -19,9 +19,9 @@
     * При перемещении окна выводить его старую и новую позицию
     * При изменении размера окна выводить его новый размер
 """
-
-
-from PySide6 import QtWidgets
+import time
+from PySide6 import QtWidgets, QtGui
+from PySide6.QtWidgets import QApplication
 from ui.c_signals_events_form import Ui_Form
 
 
@@ -32,19 +32,45 @@ class Window(QtWidgets.QWidget):
 
         self.ui = Ui_Form()
         self.ui.setupUi(self)
-        self.initSignals()
+        self.ui.pushButtonMoveCoords.clicked.connect(self.MoveCoords)
+        self.ui.pushButtonGetData.clicked.connect(self.GetDataScreen)
 
-    def initSignals(self) -> None:
-        self.pushButtonM
+    # def initSignals(self) -> None:
+    # self.ui.pushButtonMoveCoords.clicked.connect(self.OnpushButtonMoveCoords)
+
+    def MoveCoords(self):
+        y = self.ui.spinBoxY.value()
+        x = self.ui.spinBoxX.value()
+        self.move(x, y)
+        self.ui.plainTextEdit.appendPlainText(f"Окно перемещено на координаты {x}, {y}")
 
 
+    def GetDataScreen(self):
+        screen = QApplication.screens()[0]
+        data = {"Кол-во экранов": len(QApplication.screens()),
+                "Текущее основное окно": self,
+                "Разрешение экрана": [screen.size(), screen.geometry()],
+                "На каком экране окно находится": self.screen(),
+                "Размеры окна": self.size(),
+                "Минимальные размеры окна": self.minimumSize(),
+                "Текущее положение (координаты) окна": self.geometry(),
+                "Координаты центра приложения": self.geometry().center(),
+                "Отслеживание состояния окна (свернуто/развёрнуто/активно/отображено)": "активно" if self.isActiveWindow() else "свернуто"}
 
+        for key, value in data.items():
+            self.ui.plainTextEdit.appendPlainText(f"{key}: {value}")
 
+        self.ui.plainTextEdit.appendPlainText(time.ctime())
 
+    def moveEvent(self, event: QtGui.QMoveEvent):
+        oldPos = event.oldPos()
+        newPos = event.pos()
+        self.ui.plainTextEdit.appendPlainText(f"{oldPos}, {newPos}")
+        self.ui.plainTextEdit.appendPlainText(time.ctime())
 
-    def onPushButtonLineEditClicked(self) -> None:
-
-
+    def resizeEvent(self, event: QtGui.QResizeEvent):
+        resize = event.size()
+        self.ui.plainTextEdit.appendPlainText(f"{resize}")
 
 
 if __name__ == "__main__":
